6a91dc9c382ca334cd9f81bd3bc58bf1634c4d5d
zobayerdev <zobayer.dev@gmail.com>
2024-03-21 19:38:39 +0600

    new system update

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -11,9 +12,11 @@ class _HomePageState extends State<HomePage> {
   late PageController _pageController;
 
   void _onScroll() {}
+  int totalPage = 4;
 
   @override
   void initState() {
+    super.initState();
     _pageController = PageController(
       initialPage: 0,
     )..addListener(_onScroll);
@@ -26,18 +29,23 @@ class _HomePageState extends State<HomePage> {
         controller: _pageController,
         children: <Widget>[
           MakePage(
+              page: 1,
               image: 'lib/assets/one.jpg',
               title: 'Forest',
-              description: 'This is a forest description.'),
+              description:
+                  'Mangrove forests are dynamic coastal ecosystems, thriving at the intersection of land and sea. These lush expanses are characterized by salt-tolerant trees and shrubs, whose tangled roots provide critical shelter for a diverse array of marine and terrestrial species. Acting as natural coastal defenders, mangroves protect shorelines from erosion and help mitigate the impacts of climate change by sequestering significant amounts of carbon.'),
           MakePage(
+              page: 2,
               image: 'lib/assets/two.jpg',
               title: 'Beach',
               description: 'This is a beach description.'),
           MakePage(
+              page: 3,
               image: 'lib/assets/three.jpg',
               title: 'Mountain',
               description: 'This is a mountain description.'),
           MakePage(
+              page: 4,
               image: 'lib/assets/four.jpg',
               title: 'Desert',
               description: 'This is a desert description.'),
@@ -46,7 +54,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget MakePage({image, title, description}) {
+  Widget MakePage({image, title, description, page}) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -73,17 +81,17 @@ class _HomePageState extends State<HomePage> {
               const SizedBox(
                 height: 40,
               ),
-              const Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.baseline,
                 textBaseline: TextBaseline.alphabetic,
                 children: [
-                  Text("1",
+                  Text(page.toString(),
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 30,
                           fontWeight: FontWeight.bold)),
-                  Text("/4",
+                  Text("/" + totalPage.toString(),
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 15,
@@ -95,11 +103,15 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(title,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold)),
+                    Text(
+                      title,
+                      style: GoogleFonts.spaceGrotesk(
+                        textStyle: Theme.of(context).textTheme.displayLarge,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
                     const SizedBox(
                       height: 2,
                     ),
@@ -165,9 +177,31 @@ class _HomePageState extends State<HomePage> {
                     const SizedBox(
                       height: 20,
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 35),
+                      child: Text(
+                        description,
+                        style: GoogleFonts.spaceGrotesk(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     Text(
-                      description,
-                      style: TextStyle(color: Colors.white70, height: 1.9),
+                      'READ MORE',
+                      style: GoogleFonts.spaceGrotesk(
+                        textStyle: Theme.of(context).textTheme.displayLarge,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
                     )
                   ],
                 ),
